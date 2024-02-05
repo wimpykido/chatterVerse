d5e4a786cd43ab6a81de89e0d3a0bdaee5ceaa1a
Eliso Saladze <eliso.saladze5@gmail.com>
2024-02-06 01:48:05 +0400

    custom theme and dark-light modes

---
+++ src/context/theme-provider.tsx
@@ -0,0 +1,96 @@
+import {
+  ReactNode,
+  useState,
+  createContext,
+  useContext,
+  useEffect,
+} from "react";
+import {
+  ThemeProvider as MuiThemeProvider,
+  Theme,
+  createTheme,
+} from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+type Props = {
+  children: ReactNode;
+};
+
+type ThemeContextType = {
+  toggleDarkMode: () => void;
+  darkMode: boolean;
+};
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export const useThemeContext = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useThemeContext must be used within a ThemeProvider");
+  }
+  return context;
+};
+
+const ThemeProvider = ({ children }: Props) => {
+  const storedDarkMode = localStorage.getItem("darkMode");
+  const [darkMode, setDarkMode] = useState(
+    storedDarkMode ? JSON.parse(storedDarkMode) : true
+  );
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prevDarkMode: boolean) => !prevDarkMode);
+  };
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: "light",
+      primary: {
+        main: "#FFFFFF",
+        light: "#f5f5f5",
+        dark: "#00A3FF",
+        contrastText: "#515151",
+      },
+      secondary: {
+        main: "#171717",
+        light: "#f5f5f5",
+        dark: "#dfdfdf",
+        contrastText: "#FFFFFF",
+      },
+    },
+  });
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+      primary: {
+        main: "#121212",
+        light: "#181818",
+        dark: "#00A3FF",
+        contrastText: "#f5f5f5",
+      },
+      secondary: {
+        main: "#FFFFFF",
+        light: "#181818",
+        dark: "#303030",
+        contrastText: "#171717",
+      },
+    },
+  });
+
+  const theme: Theme = !darkMode ? darkTheme : lightTheme;
+
+  return (
+    <ThemeContext.Provider value={{ toggleDarkMode, darkMode }}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </MuiThemeProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
