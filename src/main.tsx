be1b5736c427f6c841c45a5c01ec97293d18f856
Eliso Saladze <eliso.saladze5@gmail.com>
2024-02-06 01:49:31 +0400

    routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ThemeProvider from "./context/theme-provider.tsx";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import SignIn from "./routes/sign-in/index.tsx";
+import SignUp from "./routes/sign-up/index.tsx";
+import Profile from "./routes/profile/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/sign-in" />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
