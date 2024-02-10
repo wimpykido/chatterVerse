5718b691c91436cd173671dabb77d3c4f59ac06a
Eliso Saladze <eliso.saladze5@gmail.com>
2024-02-11 00:22:56 +0400

    using login methods

--- src/components/continue-with/index.tsx
+++ src/components/continue-with/index.tsx
@@ -1,14 +1,41 @@
 import { Box, IconButton } from "@mui/material";
 import google from "../../assets/google.png";
 import facebook from "../../assets/facebook.png";
+import { AuthContext, AuthContextProps } from "../../context/authContext";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ContinueWith = () => {
+  const { googleSignIn, facebookSignIn, user } = useContext(
+    AuthContext
+  ) as AuthContextProps;
+  const navigate = useNavigate();
+  const handleGoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleFacebookSignIn = async () => {
+    try {
+      await facebookSignIn();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigate("/profile");
+    }
+  }, [user, navigate]);
   return (
     <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
-      <IconButton>
+      <IconButton onClick={handleGoogleSignIn}>
         <img width={"32px"} height={"32px"} src={google} alt="Icon 1" />
       </IconButton>
-      <IconButton>
+      <IconButton onClick={handleFacebookSignIn}>
         <img width={"32px"} height={"32px"} src={facebook} alt="Icon 2" />
       </IconButton>
     </Box>
