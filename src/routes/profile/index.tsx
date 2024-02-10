81a82618e40316d6feec6d7498258d7c9e2644a9
Eliso Saladze <eliso.saladze5@gmail.com>
2024-02-11 00:23:38 +0400

    test

--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { auth } from '../../firebase'
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase";
+import { useContext } from "react";
+import { AuthContext, AuthContextProps } from "../../context/authContext";
 
 const Profile = () => {
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext) as AuthContextProps;
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+      navigate("/sign-in");
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
   return (
     <div>
-      {auth.currentUser!.email}
+      <>{user!.email}</>
+      <>{user!.displayName}</>
+      <button onClick={handleLogout}>log out</button>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
