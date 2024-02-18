d4679f0651b3ec8b7e78c52f5df9c9e0455c875a
Eliso Saladze <eliso.saladze5@gmail.com>
2024-02-18 19:14:05 +0400

    context for chat

---
+++ src/context/chat-context.tsx
@@ -0,0 +1,82 @@
+import { ReactNode, createContext, useContext, useReducer } from "react";
+import { AuthContext, AuthContextProps } from "./authContext";
+
+export type Chat = {
+  id: string;
+  createdAt: number;
+  lastMessage: string;
+};
+
+type Props = {
+  children: ReactNode;
+};
+
+type ChatState = {
+  chatId: string | null;
+  chat: Chat;
+};
+
+type ChatAction = {
+  type: string;
+  payload: Chat;
+};
+
+export type ChatContextType = {
+  data: ChatState;
+  dispatch: React.Dispatch<ChatAction>;
+};
+
+export const ChatContext = createContext<ChatContextType | undefined>(
+  undefined
+);
+
+export const ChatContextProvider = ({ children }: Props) => {
+  const initialState = {
+    chatId: null,
+    chat: {} as Chat,
+  };
+
+  const authContext = useContext(AuthContext) as AuthContextProps;
+  const { user } = authContext;
+
+  const chatReducer = (state: ChatState, action: ChatAction) => {
+    switch (action.type) {
+      case "CHANGE_USER": {
+        const currentUserUid = user?.uid;
+        const otherUserId = action.payload.id;
+
+        if (currentUserUid && otherUserId) {
+          const chatId =
+            currentUserUid > otherUserId
+              ? `${currentUserUid}_${otherUserId}`
+              : `${otherUserId}_${currentUserUid}`;
+
+          console.log(chatId);
+          return {
+            ...state,
+            chat: action.payload,
+            chatId: chatId,
+          };
+        } else {
+          console.error("User IDs are not available.");
+          return state;
+        }
+      }
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(chatReducer, initialState);
+
+  return (
+    <ChatContext.Provider
+      value={{
+        data: state,
+        dispatch,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
